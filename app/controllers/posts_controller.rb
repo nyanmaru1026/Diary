a39300bd65759092522057340476cc4bfea924e2
nyanmaru1026 <nyanmaru2701@gmail.com>
2020-05-03 14:51:03 +0900

    フロント実装、createアクション作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,17 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
-  
+
+  def create
+    Post.create(post_params)
+    redirect_to root_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 
 end
